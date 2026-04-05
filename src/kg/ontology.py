15375b1aa14f84aec0278d221a0e1ec7b7102962
Danushnan Sri Gowribalan <dankri6455@gmail.com>
2026-04-05 14:38:57 +0000

    Updated GTFS extensions

--- src/kg/ontology.py
+++ src/kg/ontology.py
@@ -2,9 +2,13 @@ from rdflib import Graph, Namespace, Literal
 from rdflib.namespace import RDF, RDFS, OWL, XSD
 
 LT = Namespace("http://example.org/london-transport#")
+GTFS = Namespace("http://vocab.gtfs.org/terms#")
+SCHEMA = Namespace("https://schema.org/")
 
 NAMESPACES = {
     "lt": LT,
+    "gtfs": GTFS,
+    "schema": SCHEMA,
     "rdf": RDF,
     "rdfs": RDFS,
     "owl": OWL,
@@ -52,15 +56,36 @@ def build_ontology() -> Graph:
     for prefix, ns in NAMESPACES.items():
         g.bind(prefix, ns)
 
+    # Classes from GTFS ontology
+    add_class(g, GTFS.Stop, "Stop", "A physical location passengers use for transport", LT.TransportEntity)
+    add_class(g, GTFS.Route, "Route", "A transit route representing a group of trips", LT.TransportEntity)
+    add_class(g, GTFS.Trip, "Trip", "A single journey along a route", LT.TransportEntity)    
+    add_class(g, GTFS.Service, "Service", "A set of trips operating on specific days", LT.TransportEntity)
+    add_class(g, GTFS.StopTime, "Stop Time", "Arrival and departure times at a stop for a trip", LT.TransportEntity)
+
+    # Extend GTFS with subclasses
+    add_class(g, LT.BusRoute, "Bus Route", "A route used by buses", GTFS.Route)
+    add_class(g, LT.TrainRoute, "Train Route", "A route used by trains", GTFS.Route)
+    add_class(g, LT.BusStop, "Bus Stop", "A stop specifically for bus services", GTFS.Stop)
+    add_class(g, LT.TrainStation, "Train Station", "A station for train services", GTFS.Stop)
+
+    # GTFS properties
+    add_datatype_property(g, GTFS.stopName, "Stop Name", "The name of a stop", range_=XSD.string)
+    add_datatype_property(g, GTFS.lat, "Latitude", "The latitude coordinate of a stop", range_=XSD.float)
+    add_datatype_property(g, GTFS.long, "Longitude", "The longitude coordinate of a stop", range_=XSD.float)
+    add_datatype_property(g, GTFS.routeShortName, "Route Short Name", "The short name or number of a transit route", range_=XSD.string)
+    add_datatype_property(g, GTFS.headsign, "Headsign", "The destination text displayed to passengers on a vehicle", range_=XSD.string)
+    add_datatype_property(g, GTFS.arrivalTime, "Arrival Time", "The scheduled arrival time at a specific stop", range_=XSD.string)
+    add_datatype_property(g, GTFS.departureTime, "Departure Time", "The scheduled departure time from a specific stop", range_=XSD.string)
+
+    # GTFS subproperties
+    add_datatype_property(g, LT.naptanCode, "NaPTAN stop code", "A unique identifier assigned to a stop within the UK NaPTAN system", GTFS.Stop, XSD.string, GTFS.stopName)
+    add_datatype_property(g, LT.busRouteNumber, "Bus route number/name", "The specific identifier or number used to distinguish a London bus route", LT.BusRoute, XSD.string, GTFS.routeShortName)
+
     # Core classes
     add_class(g, LT.TransportEntity, "Transport Entity", "The root class for all London transport-related concepts.")
     add_class(g, LT.TransportOperator, "Transport Operator", "An organisation responsible for operating transport services", LT.TransportEntity)
     add_class(g, LT.TransportLine, "Transport Line", "A transport service line consisting of multiple routes", LT.TransportEntity)
-    add_class(g, LT.Route, "Route", "A transit route representing a group of trips", LT.TransportEntity)
-    add_class(g, LT.Stop, "Stop", "A physical location passengers use for transport", LT.TransportEntity)
-    add_class(g, LT.Service, "Service", "A set of trips operating on specific days", LT.TransportEntity)
-    add_class(g, LT.Trip, "Trip", "A single journey along a route", LT.TransportEntity)
-    add_class(g, LT.StopTime, "Stop Time", "Arrival and departure times at a stop for a trip", LT.TransportEntity)
     add_class(g, LT.Place, "Place", "A physical location", LT.TransportEntity)
     add_class(g, LT.Report, "Report", "A service update, incident report, or status announcement", LT.TransportEntity)
     add_class(g, LT.Statistic, "Statistic", "Quantitative data regarding ridership, performance, or usage", LT.TransportEntity)
@@ -75,11 +100,6 @@ def build_ontology() -> Graph:
     add_class(g, LT.RiverBusLine, "River Bus Line", "A transport line operating on river services", LT.TransportLine)
     add_class(g, LT.NationalRailLine, "National Rail Line", "A rail line operating as part of the UK National Rail network", LT.TransportLine)
 
-    add_class(g, LT.BusRoute, "Bus Route", "A route used by buses", LT.Route)
-    add_class(g, LT.TrainRoute, "Train Route", "A route used by trains", LT.Route)
-
-    add_class(g, LT.BusStop, "Bus Stop", "A stop specifically for bus services", LT.Stop)
-    add_class(g, LT.TrainStation, "Train Station", "A station for train services", LT.Stop)
 
     # Generic properties
     add_object_property(g, LT.relatedTo, "related to", "A general relationship between any two transport entities.")
@@ -104,13 +124,9 @@ def build_ontology() -> Graph:
     add_datatype_property(g, LT.description, "description", "A textual explanation providing additional details", parent=LT.value)
     add_datatype_property(g, LT.routeNumber, "route number", "The identifier or number used to distinguish a bus route", LT.Route, XSD.string, LT.value)
     add_datatype_property(g, LT.stopCode, "stop code", "A unique identifier assigned to a stop within the UK NaPTAN system", LT.Stop, XSD.string, LT.value)
-    add_datatype_property(g, LT.latitude, "latitude", "Latitude of a stop", LT.Stop, XSD.float, LT.value)
-    add_datatype_property(g, LT.longitude, "longitude", "Longitude of a stop", LT.Stop, XSD.float, LT.value)
     add_datatype_property(g, LT.wheelchairAccessible, "wheelchair accessible", "Indicates whether a stop is accessible to wheelchair users", LT.Stop, XSD.boolean, LT.value)
     add_datatype_property(g, LT.startDate, "start date", "The date on which a service begins operation", LT.Service, XSD.string, LT.value)
     add_datatype_property(g, LT.endDate, "end date", "The date on which a service ceases operation", LT.Service, XSD.string, LT.value)
-    add_datatype_property(g, LT.arrivalTime, "arrival time", "Arrival time at a stop", LT.StopTime, XSD.string, LT.value)
-    add_datatype_property(g, LT.departureTime, "departure time", "Departure time from a stop", LT.StopTime, XSD.string, LT.value)
     add_datatype_property(g, LT.hasRidership, "has ridership", "Data regarding the number of passengers using a service", None, XSD.string, LT.value)
     add_datatype_property(g, LT.hasFrequency, "has frequency", "Information about how often a service runs", None, XSD.string, LT.value)
     add_datatype_property(g, LT.extendsTo, "extends to", "The geographic reach or terminal destination of a service", None, XSD.string, LT.value)
